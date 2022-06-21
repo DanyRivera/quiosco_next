5c3c4b4b5a64193b2f0254e0f4d3e7226ff1753d
Dany <dany@gmail.com>
2022-06-20 22:07:14 -0500

    End 1.0

--- context/QuiscoProvider.jsx
+++ context/QuiscoProvider.jsx
@@ -1,16 +1,21 @@
 import { useState, useEffect, createContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
 const QuiscoContext = createContext();
 
 const QuiscoProvider = ({ children }) => {
+
+    const router = useRouter();
 
     const [categorias, setCategorias] = useState([]);
     const [categoriaActual, setCategoriaActual] = useState({});
     const [producto, setProducto] = useState({});
     const [modal, setModal] = useState(false);
     const [pedido, setPedido] = useState([]);
+    const [nombre, setNombre] = useState('');
+    const [total, setTotal] = useState(0);
 
     const obtenerCategorias = async () => {
         const { data } = await axios('/api/categorias');
@@ -25,9 +30,18 @@ const QuiscoProvider = ({ children }) => {
         setCategoriaActual(categorias[0])
     }, [categorias])
 
+    useEffect(() => {
+
+        const nuevoTotal = pedido.reduce((total, producto) => (producto.precio * producto.cantidad) + total, 0);
+
+        setTotal(nuevoTotal);
+
+    }, [pedido])
+
     const handleClickCategoria = id => {
         const categoria = categorias.filter(categoria => categoria.id === id);
         setCategoriaActual(categoria[0]);
+        router.push('/');
     }
 
     const handleClickProducto = producto => {
@@ -38,7 +52,7 @@ const QuiscoProvider = ({ children }) => {
         setModal(!modal);
     }
 
-    const handleAgregarPedido = ({ categoriaId, imagen, ...producto }) => {
+    const handleAgregarPedido = ({ categoriaId, ...producto }) => {
 
         if (pedido.some(p => p.id === producto.id)) {
 
@@ -79,6 +93,66 @@ const QuiscoProvider = ({ children }) => {
 
     }
 
+    const handleEditarCantidades = id => {
+
+        const productoActualizar = pedido.filter(producto => producto.id === id);
+        setModal(true);
+        setProducto(productoActualizar[0]);
+
+    }
+
+    const handleEliminarProducto = id => {
+        const pedidoActualizado = pedido.filter(producto => producto.id !== id);
+        setPedido(pedidoActualizado);
+
+        toast.success('Producto Eliminado ', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    }
+
+    const colocarOrden = async e => {
+        e.preventDefault();
+
+        try {
+
+            const data = await axios.post('/api/ordenes', { pedido, nombre, total, fecha: Date.now().toLocaleString() });
+
+            //Resetear la app
+            setCategoriaActual(categorias[0])
+            setPedido([]);
+            setNombre('');
+            setTotal(0);
+
+            toast.success('Orden Agregada ', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+
+            setTimeout(() => {
+                router.push('/');
+            }, 3000);
+
+
+        } catch (error) {
+            console.log(error);
+        }
+
+        // console.log(pedido);
+        // console.log(nombre);
+        // console.log(total);
+    }
+
     return (
         <QuiscoContext.Provider
             value={{
@@ -87,10 +161,16 @@ const QuiscoProvider = ({ children }) => {
                 producto,
                 modal,
                 pedido,
+                nombre,
+                total,
                 handleClickCategoria,
                 handleClickProducto,
                 handleChangeModal,
                 handleAgregarPedido,
+                handleEditarCantidades,
+                handleEliminarProducto,
+                setNombre,
+                colocarOrden
             }}
         >
             {children}
--- pages/total.jsx
+++ pages/total.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useCallback } from "react";
+import {formatearDinero} from "../helpers";
 import Layout from "../layout/Layout"
+import useQuiosco from "../hooks/useQuiosco";
 
 export default function Total() {
+
+    const { pedido, nombre, total, setNombre, colocarOrden } = useQuiosco();
+
+    const comprobarPedido = useCallback(() => {
+        return pedido.length === 0 || nombre === '' || nombre.length < 3
+    }, [pedido, nombre]);
+
+    useEffect(() => {
+
+        comprobarPedido()
+
+    }, [pedido, nombre, comprobarPedido]);
 
     return (
         <Layout
@@ -9,6 +24,35 @@ export default function Total() {
             <h1 className="text-4xl font-black">Total</h1>
             <p className="text-2xl my-10">Confirma tu Pedido a Continuación</p>
 
+
+            <form
+                onSubmit={colocarOrden}
+            >
+                <div>
+                    <label htmlFor="nombre" className="block uppercase text-slate-800 font-bold text-xl ">Nombre</label>
+
+                    <input
+                        type="text"
+                        id="nombre"
+                        value={nombre}
+                        onChange={e => setNombre(e.target.value)}
+                        className="bg-gray-200 w-full mt-3 lg:w-1/3 p-2 rounded-md"
+                    />
+                </div>
+
+                <div className="mt-10">
+                    <p className="text-2xl">Total a Pagar: {' '} <span className="font-bold">{formatearDinero(total)}</span></p>
+                </div>
+
+                <div className="mt-5">
+                    <input
+                        className={`${comprobarPedido() ? 'bg-indigo-100' : 'bg-indigo-600 hover:bg-indigo-800'} w-full px-5 py-2 rounded uppercase font-bold text-white text-center cursor-pointer`}
+                        value='Confirmar Pedido'
+                        type="submit"
+                        disabled={comprobarPedido()}
+                    />
+                </div>
+            </form>
 
         </Layout>
     )
